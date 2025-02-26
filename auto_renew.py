e9a02b1c250414b8c06135fcf7e4b1e06b5f51d2
jagaldol <yoy06056@pusan.ac.kr>
2025-02-26 18:08:42 +0900

    최종 티켓팅 성공 확인

--- auto_renew.py
+++ auto_renew.py
@@ -9,13 +9,8 @@ import pyautogui
 # 상태를 열거형(enum)으로 정의
 class TicketingState(enum.Enum):
     INIT = 0
-    DATE_SELECTION = 1
     LOADING = 2
-    NEXT_PAGE = 3
     NEED_REFRESH = 4
-    FOCUS_DATE = 5
-    STANDING_SELECTION = 6
-    FINISH = 7
 
 
 terminate = False
@@ -45,6 +40,20 @@ def on_press_terminate_key(e):
 keyboard.on_press_key("f9", on_press_terminate_key)
 
 
+def locate_and_click(image_path, confidence=0.8):
+    """이미지를 찾아서 클릭하는 함수. 없으면 False 반환."""
+    try:
+        location = pyautogui.locateOnScreen(image_path, confidence=confidence)
+    except pyautogui.ImageNotFoundException:
+        return False
+
+    if location:
+        center_point = pyautogui.center(location)
+        pyautogui.click(center_point)
+        return True
+    return False
+
+
 def locate_find(image_path, confidence=0.8):
     """이미지를 찾아서 클릭하는 함수. 없으면 False 반환."""
     try:
@@ -61,7 +70,7 @@ def main():
     print("공연장이 감지되면 새로고침합니다.")
     print("프로그램을 종료하려면 F9 키를 누르세요.")
 
-    state = TicketingState.NEED_REFRESH
+    state = TicketingState.INIT
     retry = 0
     last_check_time = time.time()  # 마지막 확인 시각 기록
 
@@ -70,30 +79,37 @@ def main():
             print("프로그램을 종료합니다.")
             break
 
+        if state == TicketingState.INIT:
+            if locate_and_click("targets/alert_confirm.png", confidence=0.8):
+                print("alert 확인 클릭")
+            if locate_and_click("targets/date_selector.png", confidence=0.9):
+                pyautogui.click()
+                state = TicketingState.NEED_REFRESH
+            elif locate_find("targets/selected_date_selector.png", confidence=0.9):
+                state = TicketingState.NEED_REFRESH
+
         if state == TicketingState.LOADING:
             # 좌석이 완전 없는거를 찾음(화면에 텅 빈 좌석이 보여야 새로고침 시도)
             retry += 1
-            if locate_find("targets/select_box.png", confidence=0.8):
+            if locate_find("targets/select_box.png", confidence=0.9):
                 state = TicketingState.NEED_REFRESH
 
             if retry > 20:
+                retry = 0
                 current_time = time.time()
                 elapsed = current_time - last_check_time
-                print(f"좌석 선택 화면인지 확인 - {elapsed:.2f}초")
+                print(f"초기상태로 회귀 - {elapsed:.2f}초")
                 last_check_time = current_time  # 확인 후 시각 업데이트
-                retry = 0
-                if locate_find("targets/finish.png", confidence=0.8):
-                    state = TicketingState.NEED_REFRESH
+                state = TicketingState.INIT
 
         elif state == TicketingState.NEED_REFRESH:
-            keyboard.press_and_release("enter")
-            pyautogui.doubleClick()
+            current_mouse_x, current_mouse_y = pyautogui.position()
+            pyautogui.click(600, 600)
+            pyautogui.doubleClick(current_mouse_x, current_mouse_y)
             keyboard.press_and_release("left")
             keyboard.press_and_release("right")
-
             state = TicketingState.LOADING
             retry = 0
-            time.sleep(0.01)
 
 
 if __name__ == "__main__":
